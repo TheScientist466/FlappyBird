7d0554ce884fe4e4905bff44297f05fb5789062e
TheScientist46 <swarnim.das.01@gmail.com>
2021-09-03 13:03:36 +0530

    Configuration changes and Adding the window.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
+const uint windowWidth = 960;
+const uint windowHeight = 540;
+
+sf::RenderWindow window (sf::VideoMode(windowWidth, windowHeight), "Window", sf::Style::Close);
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    
+    while(window.isOpen())
+    {
+        sf::Event e;
+        while(window.pollEvent(e))
+        {
+            if(e.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
 }
+
+
